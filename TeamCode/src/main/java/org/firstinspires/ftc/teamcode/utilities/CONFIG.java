6762c8182c79cccb4283cd7fd94bc41bad7507e2
mud-ali <96320211+mud-ali@users.noreply.github.com>
2023-09-18 21:30:04 -0400

    Add config and placeholder hardware interacting util classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/CONFIG.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.utilities;
+
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class CONFIG {
+    //drivetrain wheels :OOO
+    public static String FRONT_LEFT = "fl"; //ch 0
+    public static String FRONT_RIGHT = "fr"; //ch 2
+    public static String BACK_LEFT = "bl"; //ch 3
+    public static String BACK_RIGHT = "br"; //ch 1
+
+    //webcam
+    public static String webcam = "Webcam 1";
+
+    //subsystems
+    public static String clawServoLeft = "claw"; //ch 1
+
+    public static String clawServoRight = "clawLift"; //exp 2
+
+    public static String clawSpin = "clawSpin"; //ch 0
+
+    public static String encServo = "encServo"; //ch 2
+
+    public static String liftMotorRight = "lmr"; //exp 1
+    public static String liftMotorLeft = "lml";  //exp 0
+
+    public static String O_C = "fr"; //center odo, ch 2
+    public static String O_L = "fl"; //left odo, ch 0
+    public static String O_R = "br"; //right odo, ch 1
+
+    //OLD
+    public static String liftMotor = "lm"; //
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/MecanumDrive.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.utilities;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public class MecanumDrive {
+
+    public MecanumDrive(HardwareMap hmap) {
+        fl = hmap.get(DcMotor.class, CONFIG.FRONT_LEFT);
+        fr = hmap.get(DcMotor.class, CONFIG.FRONT_RIGHT);
+        bl = hmap.get(DcMotor.class, CONFIG.BACK_LEFT);
+        br = hmap.get(DcMotor.class, CONFIG.BACK_RIGHT);
+
+        fl.setDirection(DcMotorSimple.Direction.REVERSE);
+        bl.setDirection(DcMotorSimple.Direction.FORWARD);
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        directions.put(fl, new double[]{1, 1});
+        directions.put(fr, new double[]{1, -1});
+        directions.put(bl, new double[]{1, -1});
+        directions.put(br, new double[]{1, 1});
+    }
+
+    public static DcMotor fl, fr, bl, br;
+
+    public static HashMap<DcMotor, double[]> directions = new HashMap<>();
+
+    public void move(double x, double y, double turn) {
+
+        // dot of fl and br
+        double dot_fl = dot(Objects.requireNonNull(directions.get(fl)), new double[]{x, y}) + turn;
+        double dot_fr = (dot(Objects.requireNonNull(directions.get(fr)), new double[]{x, y}) + turn);
+        double dot_bl = dot(Objects.requireNonNull(directions.get(bl)), new double[]{x, y}) - turn;
+        double dot_br = dot(Objects.requireNonNull(directions.get(br)), new double[]{x, y}) - turn;
+
+        double max = Math.max(1, Math.max(Math.max(Math.abs(dot_fl), Math.abs(dot_fr)), Math.max(Math.abs(dot_bl), Math.abs(dot_br))));
+        fl.setPower(dot_fl / max);
+        br.setPower(dot_br / max);
+
+        fr.setPower(dot_fr / max);
+        bl.setPower(dot_bl / max);
+    }
+
+    // Each double[] will be a direction vector of length two
+    public double dot(double[] a, double[] b) {
+        return a[0] * b[0] + a[1] * b[1];
+    }
+
+
+}
