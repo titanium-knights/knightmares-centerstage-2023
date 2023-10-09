0b83f3f546e77ae3ca63366cde135b4fb4c8bc17
gerinome <danielgtravels@gmail.com>
2023-10-09 09:49:28 -0400

    edited some of claw, wrote lift, added brake mode to mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Claw.java
@@ -7,44 +7,53 @@ public class Claw {
     Servo clawOpener;
     Servo clawRotator;
 
-    boolean isOpen = false;
+    boolean state = false;
+    // True = dropping move, False = Pickup
+
+
+    double zeroAngle = 0;
+    //Angle in degrees of bar such that claw is pointing straight down at pickup
+
     double angle = 0;
+
 
     public Claw(HardwareMap hmap){
         this.clawOpener = hmap.servo.get(CONFIG.clawServo);
         this.clawRotator = hmap.servo.get(CONFIG.clawSpin);
     }
 
-    public boolean isOpen() {
-        return isOpen;
-    }
-
     public double getPosition() {
         return clawOpener.getPosition();
     }
 
+    public void setPosition(double des){
+        angle = des;
+        clawRotator.setPosition(des/360);
+    }
     public void open() {
-        isOpen = true;
         clawOpener.setPosition(1.0);
     }
 
     public void close() {
-        isOpen = false;
         clawOpener.setPosition(0.0);
     }
 
-    public void turnClaw(double radians) {
-        double degrees = Math.toDegrees(radians);
-        clawRotator.setPosition(degrees/360);
-    }
-
     public void maintain(double liftAngle) {
+        double trueAngle = liftAngle + zeroAngle;
         final double MAX_BUFFER = 2.0;
         double theta = 180 - Math.toDegrees(liftAngle);
         double phi = 180 - angle;
         while (Math.abs(theta + phi - 240.0) > MAX_BUFFER) {
             double delta = (240.0 - theta - 180.0) * -1.0;
-            turnClaw(Math.toRadians(delta));
+            setPosition(delta);
         }
+    }
+
+    public void maintainDrop(double liftAngle){
+
+    }
+
+    public void maintainPickup(double liftAngle){
+        setPosition(180 - liftAngle);
     }
 }
