46462e1e077c10f91f6f715d88413e0e68664fe6
gerinome <danielgtravels@gmail.com>
2023-10-10 14:04:53 -0400

    new lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Lift.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.utilities;
 
+import static java.lang.Double.max;
+import static java.lang.Double.min;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -7,58 +10,102 @@ public class Lift {
     public DcMotor liftMotor;
 
     public static double LIFT_POWER_MULTIPLIER = .9;
+    public static double Encoder_Ticks = 537.7;
+    public static double Up_Angle = 55;
+    //Angle from initial when arm is straight up.
 
     // TODO check limit values based on degrees,
-    // should be between ~ -45 & 135 degrees
-    public static double MAX_LIMIT = 0.65;
-    public static double MIN_LIMIT = 0.01;
+    // should be between ~ -45 & 180 degrees
+    public static double MAX_LIMIT = 240;
+    public static double MIN_LIMIT = -5;
 
     //these might end up being the same as min and max limit
     public static double PICKUP_POINT = 0.1;
     public static double DROP_POINT = 0.60;
 
     // higher power lift going up vs going down cause power + gravity = more power
+    //Not going to use due to difficulty (unless someone comes up w something)
     public static double POWER_UP = 0.4;
     public static double POWER_DOWN = 0.3;
 
+    boolean state;
+    //True = on drop zone, false = on pickup zone
 
     public Lift(HardwareMap hmap) {
         this.liftMotor = hmap.dcMotor.get(CONFIG.liftMotor);
-    }
-
-    public void setPower(double power) {
-        liftMotor.setPower(-power * LIFT_POWER_MULTIPLIER);
-    }
-
-    public void setPosition(int pos, double power) {
-        liftMotor.setTargetPosition(pos);
-        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftMotor.setPower(power);
+        this.state = true;
+        setInit();
     }
 
     public void setInit() {
+        liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    public int getPosition() {
-        return liftMotor.getCurrentPosition();
+    public void stop(){
+        liftMotor.setPower(0);
+        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    public void runToPosition(int pos) {
-        runToPosition(pos, LIFT_POWER_MULTIPLIER);
+    public boolean checkLimits(){
+        double angle = getPosition();
+        if (angle < MIN_LIMIT) {stop(); return true;}
+        if (angle > MAX_LIMIT) {stop(); return true;}
+        return false;
     }
 
-    public void runToPosition(int pos, double multiplier){
-        int currentPos = liftMotor.getCurrentPosition();
+    public boolean updateState(){
+        if (getPosition() > Up_Angle) state = false;
+        state = true;
+        return state;
+    }
 
-        //consider creating a more fine grained control loop
-        if(pos - currentPos > 30){
-            setPower(-1 * multiplier);
-        } else if(pos - currentPos < -30){
-            setPower(multiplier);
-        } else {
-            setPower(0);
-        }
+    public boolean isBusy() {return liftMotor.isBusy();}
+
+    public void setPower(boolean dir) {
+        //false = to pick up, true = to drop
+        if (dir) liftMotor.setPower(POWER_DOWN * LIFT_POWER_MULTIPLIER);
+        else liftMotor.setPower(-POWER_DOWN * LIFT_POWER_MULTIPLIER);
+    }
+
+    public double getPosition(){
+        return (liftMotor.getCurrentPosition()*360/Encoder_Ticks);
+    }
+
+    public boolean runToPosition(double angle) {
+        //just in case wrong input
+        double orig = angle;
+        angle = min(angle, MAX_LIMIT);
+        angle = max(angle, MIN_LIMIT);
+        if (orig != angle) return false;
+
+        liftMotor.setTargetPosition((int) (Encoder_Ticks*angle/360));
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //with run to position always positive power
+        setPower(true);
+
+        return true;
+    }
+
+    public boolean toScan(){
+        return runToPosition(45);
+    }
+
+    public boolean toBottomPreset(){
+        return runToPosition(0);
+    }
+
+    public void toDrop(){
+        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        if (checkLimits()) return;
+        setPower(true);
+
+    }
+
+    public void toPickUp(){
+        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        if (checkLimits()) return;
+        setPower(false);
     }
 }
