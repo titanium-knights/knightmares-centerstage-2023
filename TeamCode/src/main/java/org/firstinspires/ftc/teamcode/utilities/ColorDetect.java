b5141e0e5b057e1b54805b51d92f379a833e6800
iDrg899 <omnom899@gmail.com>
2023-10-16 17:19:11 -0400

    ColorDetect takes HardwareMap as parameter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/ColorDetect.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/ColorDetect.java
@@ -8,12 +8,14 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 @Config
 public class ColorDetect {
     ColorSensor colorSensor;
 
-    public ColorDetect(ColorSensor colorSensor) {
-        this.colorSensor = colorSensor;
+    public ColorDetect(HardwareMap hMap) {
+        this.colorSensor = hMap.get(ColorSensor.class, "color");
     }
 
     public int[] readRGB() {
