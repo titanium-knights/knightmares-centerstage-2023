ee5aeaa50d92574fc67875e4851f0395326839bd
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-10-25 18:16:14 -0400

    fix missing curly brace

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/PlaneLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/PlaneLauncher.java
@@ -10,7 +10,7 @@ public class PlaneLauncher {
 
     public PlaneLauncher(HardwareMap hmap) {
         this.launcher = hmap.servo.get(CONFIG.clawServo);
-
+    }
     // when releasing or resetting just move the servo...
     public void release(){
         launcher.setPosition(0.2);
