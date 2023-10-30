cee595361c14a54bb9180fc3871bd2495027070d
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-10-30 13:29:38 -0400

    mudasir edited comments up until getPosition() in Lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Lift.java
@@ -18,13 +18,12 @@ public class Lift {
     in other words, it will turn 28 times slower
     that means in one motor rotation, there will be 537.6/28 encoder ticks */
 
+    // angle of vertical arm from robot arm's minimum position
+    //TODO: consider deleting unused variable
     public static double VERTICAL_ANGLE = 135;
-    // Angle from when arm is vertical, where 0 is the minimum degree that the robot arm can go down (robot centric based)
-    // uhhhhhh i dont think we even use this
 
-    // TODO: check limit values based on degrees, should be between ~ -45 & 180 degrees
-    public static double MAX_LIMIT = 240; // so it doesn't go too far down, measured in degrees based on where the
-    public static double MIN_LIMIT = -5; // stops it from going further down if we use manual lift
+    public static double MAX_LIMIT = 240; // upper limit
+    public static double MIN_LIMIT = -5; // lower limit for manual lift
 
     //these might end up being the same as min and max limit
     public static double PICKUP_POINT = 0.1; // tells the arm thing where to go
@@ -35,12 +34,12 @@ public class Lift {
     public static double POWER_UP = 0.75;
     public static double POWER_DOWN = 0.55;
 
-    boolean onDropZone;
-    //True = on drop zone, false = on pickup zone
+    //True = at drop zone, false = at pickup zone
+    boolean atDropZone;
 
     public Lift(HardwareMap hmap) {
         this.liftMotor = hmap.dcMotor.get(CONFIG.liftMotor);
-        this.onDropZone = true;
+        this.atDropZone = true;
         setInit();
     }
 
@@ -50,34 +49,39 @@ public class Lift {
         liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
-    public void stop(){ // sets power to 0, everything stops as a result
+    //completely stop lift
+    public void stop() {
         liftMotor.setPower(0);
         liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
+    // purpose: make sure you don't interrupt a preset
     public boolean isBusy() {
-        return liftMotor.isBusy(); // used to make sure you don't interrupt a preset
+        return liftMotor.isBusy();
     }
 
-    public void setPower(boolean dir) { // subset of the method below
-        // only for where normal mode is on
+    // overload the other setPower to default slowmode to false
+    public void setPower(boolean dir) {
         setPower(dir, false);
     }
 
+    /**
+     * @param dir: false = to pick up, true = to drop
+     * @param slowMode: false = normal mode, true = slow mode
+     */
     public void setPower(boolean dir, boolean slowMode) {
-        //dir (direction): false = to pick up, true = to drop
-        // slowMode: false = normal mode, true = slow mode
-
-        // if slowMode is on, use power down constant, else, use power up constant
+        //TODO: rename to slow_power and power because we're not actually using them for up/down
         double power = slowMode ? POWER_DOWN : POWER_UP;
+        // if slowMode is on, use power down constant, else, use power up constant
         if (dir) liftMotor.setPower(power * LIFT_POWER_MULTIPLIER);
         else liftMotor.setPower(-power * LIFT_POWER_MULTIPLIER);
     }
 
-    public double getPosition(){
-        return (liftMotor.getCurrentPosition()*360/ ENCODER_TICKS);
-    } // Initially gets back position in terms of encoder ticks, which converts to degrees
     // Returns lift position in degrees, robot centric (minimum is 0)
+    public double getPosition(){
+        // Initially gets back position in terms of encoder ticks, which converts to degrees
+        return (liftMotor.getCurrentPosition()*360/ ENCODER_TICKS);
+    }
 
     public boolean runToPosition(double angle) {
         // takes the angle we want it to go to and makes sure the angle is within range
