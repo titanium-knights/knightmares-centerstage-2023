b5e1dda2a6196cb707edbed701c55d16bfc68b5b
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-11-01 22:19:05 -0400

    outline for config file for reset teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopconfigs/resetconfig.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.teleopconfigs;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class resetconfig extends TeleopConfig {
+
+    public resetconfig(Gamepad gamepad1, Gamepad gamepad2) {
+        super(gamepad1, gamepad2);
+    }
+
+    @Override
+    public void check() {
+        super.check();
+        //here you can override certain controls
+        // make sure to remove the buttons from other controls if you add it something else
+        clawClose = this.gamepad1.x;
+        //TODO: add more controls here for reset teleop
+    }
+}
