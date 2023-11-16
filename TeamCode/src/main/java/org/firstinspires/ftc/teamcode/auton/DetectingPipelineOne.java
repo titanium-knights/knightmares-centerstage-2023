58ac6d4ed62d9f9b894f2ee34deff2d6555a2af4
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-11-16 18:03:25 -0500

    worked on detection auton movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/DetectingPipelineOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/DetectingPipelineOne.java
@@ -5,26 +5,71 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.utilities.Claw;
+import org.firstinspires.ftc.teamcode.utilities.InitialVision;
+import org.firstinspires.ftc.teamcode.utilities.Lift;
 import org.firstinspires.ftc.teamcode.utilities.MecanumDrive;
 
-//Red Right Side
+//blue Right Side
 @Autonomous(name="VisionRedMaybe", group="Linear OpMode")
 @Config
 public class DetectingPipelineOne extends LinearOpMode {
-
-
+    public static final double POWER = 0.6;
+    public static final long FOOT = 500;
+    public static final long TURN_45 = 300;
+    MecanumDrive drivetrain;
 
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Initialized: ", "Hopefully");
         telemetry.update();
 
-        final double POWER = 0.85;
+        InitialVision vision = new InitialVision(hardwareMap, telemetry, "blue");
         ElapsedTime runtime = new ElapsedTime();
-        MecanumDrive drivetrain = new MecanumDrive(hardwareMap);
+        Lift lift = new Lift(hardwareMap);
+        Claw claw = new Claw(hardwareMap);
+        claw.close();
+        drivetrain = new MecanumDrive(hardwareMap);
 
         waitForStart();
         runtime.reset();
+
+        lift.toScan();
+        sleep(FOOT/2);
+        int place = vision.getPosition();
+
+        //y is forward x is left
+        switch (place) {
+            case 1:
+                drivetrain.move(0, POWER, 0);
+                sleep(FOOT*2);
+                stopDrive();
+                drivetrain.move(0, 0, POWER);
+                sleep(TURN_45*2);
+                stopDrive();
+                claw.open();
+                sleep(300);
+                drivetrain.move(-POWER, 0, 0);
+                sleep((long) (FOOT*1.5));
+                stopDrive();
+                drivetrain.move(0, POWER, 0);
+                sleep((long) (FOOT*3.5));
+                stopDrive();
+                break;
+            case 2:
+                drivetrain.move(0, POWER, 0);
+                sleep(FOOT*2);
+                stopDrive();
+                claw.open();
+                sleep(300);
+                drivetrain.move(0, POWER, 0);
+                sleep((long) (FOOT*1.5));
+                stopDrive();
+                drivetrain.move(0, 0, POWER);
+                sleep(TURN_45*2);
+                stopDrive();
+            default:
+        }
 
         drivetrain.move(-POWER, POWER, 0);
 
@@ -45,4 +90,10 @@ public class DetectingPipelineOne extends LinearOpMode {
         telemetry.update();
 
     }
+
+    public void stopDrive() {
+        drivetrain.move(0, 0, 0);
+        sleep(100);
+    }
+
 }
