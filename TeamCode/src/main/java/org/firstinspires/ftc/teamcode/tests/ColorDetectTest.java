92d5069acde3312a3c94ed969bfa1bab285c0320
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-11-18 18:11:11 -0500

    move test auton to different folder for organization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ColorDetectTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ColorDetectTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.auton;
+package org.firstinspires.ftc.teamcode.tests;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
