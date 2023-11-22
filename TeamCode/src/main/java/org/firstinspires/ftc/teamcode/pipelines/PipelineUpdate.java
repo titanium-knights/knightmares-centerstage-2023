0a934fc4fda8483b1cbe5bb5b6cb9e151eb7c392
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-11-22 09:21:56 -0500

    ignore first channel of Mat

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/PipelineUpdate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/PipelineUpdate.java
@@ -10,27 +10,29 @@ import java.util.ArrayList;
 
 public class PipelineUpdate extends OpenCvPipeline {
     Mat YCrCb = new Mat();
-    Mat Y = new Mat();
+    Mat Cr = new Mat();
+    Mat Cb = new Mat();
     Mat RectA_Y = new Mat();
     int avg;
-    int avgA;
+    int avgBlue;
 
     static final int STREAM_WIDTH = 1280;
     static final int STREAM_HEIGHT = 720;
-
+    ArrayList<Mat> yCrCbChannels = new ArrayList<Mat>(3);
     static final int WidthRectA = 130;
     static final int HeightRectA = 110;
 
     static final Point RectATopLeftAnchor = new Point(
-            (STREAM_WIDTH - WidthRectA)/2 + 300, //modify this number to move the rect
-            (STREAM_HEIGHT - HeightRectA)/2 - 100 //modify this number to move the rect
+            (STREAM_WIDTH - WidthRectA)/3.0 + 300, //modify this number to move the rect
+            (STREAM_HEIGHT - HeightRectA)/3.0 - 100 //modify this number to move the rect
     );
 
     void inputToY(Mat input) {
         Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
         ArrayList<Mat> yCrCbChannels = new ArrayList<Mat>(3);
         Core.split(YCrCb, yCrCbChannels);
-        Y = yCrCbChannels.get(0);
+        Cr = yCrCbChannels.get(1);
+        Cb = yCrCbChannels.get(2);
     }
 
     @Override
@@ -42,9 +44,14 @@ public class PipelineUpdate extends OpenCvPipeline {
     public Mat processFrame(Mat input) {
         inputToY(input);
         System.out.println("Processing requested");
-        avg = (int) Core.mean(Y).val[0];
+        avg = (int) Core.mean(Cr).val[0];
+        avgBlue = (int) Core.mean(Cb).val[0];
         YCrCb.release();
-        Y.release();
+        Cr.release();
+        Cb.release();
+
+
+
         return input;
     }
 
