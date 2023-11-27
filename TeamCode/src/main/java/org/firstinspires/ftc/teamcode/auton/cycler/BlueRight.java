98cf87435e713f96cca1604e54e5f15704e2f10d
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-11-26 19:42:22 -0500

    create folder structure template for a scoring auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/cycler/BlueRight.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.auton.cycler;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class BlueRight extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // STEPS:
+        //1. detect
+        //2. move to the spike and place the purple pixel
+        //3. score the yellow pixel in the backdrop
+        //MAYBE 4. get white pixels and cycle to backstage and/or backdrop
+
+        telemetry.addLine("this opmode does not exist");
+        telemetry.update();
+    }
+}
